9b2308f4e749447038d52c5ecf6662f8a679f10d
favxkamal <137747915+favxkamal@users.noreply.github.com>
2023-08-03 17:38:04 -0500

    Update gateway.tf

--- gateway.tf
+++ gateway.tf
@@ -1,29 +1,30 @@
-resource "aviatrix_gateway" "gateway_10" {
+resource "aviatrix_gateway" "gateway_3" {
     single_az_ha = true
-    gw_name = "FQDN-EGRESS-GATEWAY-1"
-    azure_eip_name_resource_group ="FQDNTESTPUBLICIP:rg-av-EH-Transit-399173"
+    gw_name = "FQDN-GATEWAY-REAL"
+    azure_eip_name_resource_group ="FQDN-TEST-PUBLIC-IP:rg-av-leapfrog-vpc-368676"
     allocate_new_eip =false
-    eip = "52.165.164.121"
-    vpc_id = "EH-Transit:rg-av-EH-Transit-399173:7e303da3-2c8b-4884-9e12-d348a50dd7b1"
+    eip = "20.109.248.56"
+    vpc_id = "leapfrog-vpc:rg-av-leapfrog-vpc-368676:45ed9643-2555-455a-a4b1-3db5c19233eb"
     cloud_type = 8
-    vpc_reg = "East US"
+    vpc_reg = "Central US"
     gw_size = "Standard_D2_v2"
-    account_name = "fk-Azure"
-    subnet = "10.10.66.32/28"
-    fqdn_lan_cidr = "10.10.66.80/28"
+    account_name = "fk-azure"
+    subnet = "10.13.0.0/28"
+    fqdn_lan_cidr = "10.13.0.144/28"
 }
 
-resource "aviatrix_gateway" "gateway_11" {
+resource "aviatrix_gateway" "gateway_4" {
     single_az_ha = true
-    gw_name = "FQDN-EGRESS-GATEWAY-2"
-    azure_eip_name_resource_group ="FQDNTESTPUBLICIP-1:rg-av-EH-Transit-399173"
+    gw_name = "FQDN-GATEWAY-REAL-2"
+    azure_eip_name_resource_group ="FQDN-TEST-PUBLIC-IP-2:rg-av-leapfrog-vpc-368676"
     allocate_new_eip =false
-    eip = "104.43.212.92"
-    vpc_id = "EH-Transit:rg-av-EH-Transit-399173:7e303da3-2c8b-4884-9e12-d348a50dd7b1"
+    eip = "20.221.80.7"
+    vpc_id = "leapfrog-vpc:rg-av-leapfrog-vpc-368676:45ed9643-2555-455a-a4b1-3db5c19233eb"
     cloud_type = 8
-    vpc_reg = "East US"
+    vpc_reg = "Central US"
     gw_size = "Standard_D2_v2"
-    account_name = "fk-Azure"
-    subnet = "10.10.66.48/28"
-    fqdn_lan_cidr = "10.10.66.144/28"
+    account_name = "fk-azure"
+    subnet = "10.13.0.16/28"
+    fqdn_lan_cidr = "10.13.0.208/28"
+
 }
